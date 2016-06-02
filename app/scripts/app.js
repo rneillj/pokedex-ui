39d3670ed6e9147f3248424788fc4625a4caaa4f
censhin <censhin@gmail.com>
2016-06-01 20:33:21 -0500

    feat(app): got react up and running

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,14 @@
-console.log('test');
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component {
+  render() {
+    return (
+      <h1>Pokedex</h1>
+    );
+  }
+}
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<Layout/>, app);
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -2,29 +2,31 @@ var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
 
 module.exports = {
-    context: __dirname,
-    devtool: debug ? "inline-sourcemap" : null,
-    entry: "./scripts/app.js",
-    module: {
-        loads: [
-            {
-                test: /\.js?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['react', 'es2015'],
-                    plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
-                }
-            }
-        ]
-    },
-    output: {
-        path: __dirname,
-        filename: "pokedex.min.js"
-    },
-    plugins: debug ? [] : [
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
+  context: __dirname,
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./scripts/app.js",
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
     ]
+  },
+  output: {
+    path: __dirname,
+    filename: "pokedex.min.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({mangle: false, sourcemap: false})
+  ]
 };
