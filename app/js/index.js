a4d8903f2b016f20d1030f8d3ea8b007d2f4dbae
censhin <censhin@gmail.com>
2016-06-05 00:34:56 -0500

    chore(router): changed router history to hashHistory from browserHistory to be able to share links

--- app/js/index.js
+++ app/js/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router, Route, IndexRoute, browserHistory } from "react-router";
+import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
 import Layout from "./Layout";
 import Pokedex from "./Pokedex";
@@ -10,7 +10,7 @@ import Pokemon from "./Pokemon";
 const app = document.getElementById('app');
 
 ReactDOM.render(
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
     <Route path="/" component={Layout}>
       <IndexRoute component={Pokedex} />
       <Route path="pokemon" component={PokemonList} />
